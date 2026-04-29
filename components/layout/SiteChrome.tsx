cfdca35d2c9dac504dcb0540566f165f2d2464e7
Alexandre DeSouza <alexsouzadev@gmail.com>
2026-04-29 17:16:10 +0100

    .

--- components/layout/SiteChrome.tsx
+++ components/layout/SiteChrome.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { CursorProvider } from '@/components/cursor/CursorProvider'
-import { PageTransition } from '@/components/motion/PageTransition'
 import { SiteNav } from '@/components/layout/SiteNav'
 import { SpotlightLayer } from '@/components/ui/SpotlightLayer'
 
@@ -10,7 +9,7 @@ export function SiteChrome({ children }: { children: React.ReactNode }) {
     <CursorProvider>
       <SpotlightLayer />
       <SiteNav />
-      <PageTransition>{children}</PageTransition>
+      {children}
     </CursorProvider>
   )
 }
